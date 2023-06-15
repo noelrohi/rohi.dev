5b846fec560a2645300102a70e2d1cf1ec78d9f9
gneiru <noelrohi59@gmail.com>
2023-06-15 15:58:33 +0800

    add t3env

---
+++ env.mjs
@@ -0,0 +1,19 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+ 
+export const env = createEnv({
+  server: {
+    // DATABASE_URL: z.string().url(),
+    // OPEN_AI_API_KEY: z.string().min(1),
+    MAL_CLIENT_ID: z.string().min(1),
+  },
+  client: {
+    // NEXT_PUBLIC_PUBLISHABLE_KEY: z.string().min(1),
+  },
+  runtimeEnv: {
+    // DATABASE_URL: process.env.DATABASE_URL,
+    // OPEN_AI_API_KEY: process.env.OPEN_AI_API_KEY,
+    // NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
+    MAL_CLIENT_ID: process.env.MAL_CLIENT_ID,
+  },
+});
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,26 @@
+import "./env.mjs";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.myanimelist.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.lanyard.rest',
+      },
+    ],
   },
 }
 
