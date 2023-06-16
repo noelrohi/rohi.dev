89df110ac9c6827d45cd2050bcb113d1054cb8e8
gneiru <noelrohi59@gmail.com>
2023-06-16 16:11:45 +0800

    + dayjs relativetime

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,9 @@
 import { clsx, type ClassValue } from "clsx"
 import dayjs from "dayjs"
+import relativeTime from "dayjs/plugin/relativeTime"
 import { twMerge } from "tailwind-merge"
+
+dayjs.extend(relativeTime)
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -16,3 +19,17 @@ export const getAge = () =>
     dayjs().diff("1999-09-03", "milliseconds") /
     (365.25 * 24 * 60 * 60 * 1000)
   ).toFixed(9)
+
+export function formatDate(input: string | number): string {
+  const date = new Date(input)
+  return date.toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  })
+}
+
+export function getRelativeTime(input: string | number): string {
+  const date = new Date(input)
+  return dayjs(date).fromNow()
+}
