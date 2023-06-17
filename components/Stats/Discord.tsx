3a78e1788a5234d46d5da2175dd41c75abaf8b69
gneiru <noelrohi59@gmail.com>
2023-06-17 09:18:35 +0800

    move discord component to stats

--- components/Stats/Discord.tsx
+++ components/Stats/Discord.tsx
@@ -4,12 +4,12 @@ import { lanyard } from "@/lib/api"
 import { capitalize, cn } from "@/lib/utils"
 import { Card, CardContent } from "@/components/ui/card"
 
-import { Icons } from "./icons"
+import { Icons } from "../icons"
 
 export async function DiscordCard() {
   const { discord_user: user, discord_status: status } = await lanyard()
   return (
-    <Card className="w-[350px]">
+    <Card className="w-[350px] hover:border-2 hover:border-black dark:hover:border-white">
       <CardContent className="flex justify-between py-4">
         <div className="flex flex-col space-y-2">
           <div>{user.username}</div>
