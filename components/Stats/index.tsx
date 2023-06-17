bcde2b71d7adfa74f998e34ad1658341dce7fc44
gneiru <noelrohi59@gmail.com>
2023-06-17 13:51:05 +0800

    count param for card skeleton

--- components/Stats/index.tsx
+++ components/Stats/index.tsx
@@ -31,17 +31,21 @@ export function ItemCard({
   )
 }
 
-export function SkeletonCard() {
-  return (
-    <Card className="w-[350px] hover:border-2 hover:border-black dark:hover:border-white">
+export function SkeletonCard({ count }: { count?: number }) {
+  const skeletons = Array.from({ length: count || 1 }).map((_, i) => (
+    <Card
+      className="w-[350px] hover:border-2 hover:border-black dark:hover:border-white"
+      key={i}
+    >
       <CardHeader>
         <CardTitle>
-          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[100px]" />
         </CardTitle>
       </CardHeader>
       <CardContent>
         <Skeleton className="h-4 w-[250px]" />
       </CardContent>
     </Card>
-  )
+  ))
+  return <>{skeletons}</>
 }
