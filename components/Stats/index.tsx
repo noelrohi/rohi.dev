a593f6d6965cc7f88190946cd0b7b7958f47cd71
gneiru <noelrohi59@gmail.com>
2023-06-17 14:09:41 +0800

    Revert "skeleton card"
    This reverts commit f20300c6083053c2a727413156703d2d35fd6a28.

--- components/Stats/index.tsx
+++ components/Stats/index.tsx
@@ -1,8 +1,7 @@
-import { ReactNode } from "react"
 import Link from "next/link"
+import { ReactNode } from "react"
 
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card"
-import { Skeleton } from "../ui/skeleton"
 
 export function ItemCard({
   link,
@@ -12,15 +11,11 @@ export function ItemCard({
 }: {
   link: string
   title: string
-  value: ReactNode
+  value: ReactNode,
   newtab?: boolean
 }) {
   return (
-    <Link
-      href={link}
-      target={newtab == false ? "" : "_blank"}
-      rel="noopener noreferrer"
-    >
+    <Link href={link} target={newtab == false ? "" : "_blank"} rel="noopener noreferrer">
       <Card className="w-[350px] hover:border-2 hover:border-black dark:hover:border-white">
         <CardHeader>
           <CardTitle>{title}</CardTitle>
