16869a64a3273e1b4289f69a2b9677e23de637cf
gneiru <noelrohi59@gmail.com>
2023-06-20 12:42:04 +0800

    fix svg props

--- components/icons.tsx
+++ components/icons.tsx
@@ -27,10 +27,10 @@ export const Icons = {
     <svg
       stroke="currentColor"
       fill="none"
-      stroke-width="2"
+      strokeWidth="2"
       viewBox="0 0 24 24"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       height="1em"
       width="1em"
       xmlns="http://www.w3.org/2000/svg"
