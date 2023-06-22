a1b0d72554bdd58ea4c2d544213b04635bb2a75a
gneiru <noelrohi59@gmail.com>
2023-06-22 13:55:32 +0800

    balancer

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from "react"
 import Image from "next/image"
 import Link from "next/link"
+import Balancer from "react-wrap-balancer"
 
 import { me, siteConfig } from "@/config/site"
 import { Projects } from "@/components/Projects"
@@ -10,39 +11,44 @@ export const runtime = "edge"
 export default function IndexPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <section className="flex flex-row items-center justify-between gap-4">
-        <div>
-          <Image src="/me.png" width={640} height={500} alt={siteConfig.name} />
-        </div>
-        <div>
+      <section className="grid grid-cols-1 items-center gap-4 md:grid-cols-3">
+        <Image
+          src="/me.png"
+          alt={siteConfig.name}
+          width={640}
+          height={400}
+          className="hidden aspect-auto h-auto w-auto object-cover transition-all hover:scale-105 md:block"
+          unoptimized
+        />
+        <div className="md:col-span-2">
           <div>
-            <div>
-              <div className="text-xl leading-none sm:text-2xl sm:leading-none">
-                Hi there,
-              </div>
-              <h1 className="text-xl font-extrabold leading-tight sm:text-2xl sm:leading-tight md:text-4xl md:leading-tight">
-                I&rsquo;m{" "}
-                <span className="animate-heading bg-gradient-to-r from-red-500 to-amber-400 bg-[length:200%] bg-clip-text text-transparent">
-                  {me.name}
-                </span>
-              </h1>
+            <div className="text-3xl leading-none sm:text-4xl sm:leading-tight">
+              Hi there,
             </div>
+            <h1 className="text-3xl font-extrabold leading-none sm:text-4xl sm:leading-tight">
+              I&rsquo;m{" "}
+              <span className="animate-heading bg-gradient-to-r from-red-500 to-amber-400 bg-[length:200%] bg-clip-text text-transparent">
+                {me.name}
+              </span>
+            </h1>
           </div>
-          <div className="pt-2 text-xs opacity-90 dark:opacity-80 md:text-sm">
-            I&rsquo;m a Software Engineer based in Philippines, building
-            full-stack web applications with React, Node.js, TypeScript, and
-            MySQL. I like to play games & watch anime and kdrama.
-          </div>
-          <div className="text-xs opacity-90 dark:opacity-80 md:text-sm ">
-            I&rsquo;m currently working at{" "}
-            <Link
-              href="https://www.capex.com.ph/"
-              className="underline decoration-blue-500 decoration-wavy"
-            >
-              Cargo Padala Express
-            </Link>{" "}
-            as a Web Application Developer (FullStack).
-          </div>
+          <Balancer>
+            <div className="pt-2 text-xs opacity-90 dark:opacity-80 md:text-sm">
+              I&rsquo;m a Software Engineer based in Philippines, building
+              full-stack web applications with React, Node.js, TypeScript, and
+              MySQL. I like to play games & watch anime and kdrama.
+            </div>
+            <div className="text-xs opacity-90 dark:opacity-80 md:text-sm ">
+              I&rsquo;m currently working at{" "}
+              <Link
+                href="https://www.capex.com.ph/"
+                className="underline decoration-blue-500 decoration-wavy"
+              >
+                Cargo Padala Express
+              </Link>{" "}
+              as a Web Application Developer (FullStack).
+            </div>
+          </Balancer>
         </div>
       </section>
       <Suspense fallback={<>Loading projects ...</>}>
