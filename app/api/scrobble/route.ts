25b830f1a25020136486529a801e6dea9eb3f838
gneiru <noelrohi59@gmail.com>
2023-06-23 09:03:11 +0800

    am dumb, using referer instead

--- app/api/scrobble/route.ts
+++ app/api/scrobble/route.ts
@@ -1,13 +1,12 @@
 import { NextRequest, NextResponse } from "next/server"
 import { spotify } from "@/lib/api"
 import { revalidateTag } from "next/cache"
-import { myEnv } from "@/lib/utils"
 
 export const runtime = "edge"
 
 export async function GET(req: NextRequest) {
-  const host = req.headers.get('host')
-  if(host !== myEnv.NEXT_PUBLIC_APP_URL.split('//')[1]) return new Response("Unauthorized", { status: 401 })
+  const ref = req.headers.get('referer')
+  if(!ref?.endsWith('/dashboard')) return new Response("Unauthorized", { status: 401 })
   const tracks = await spotify.recentTracks()
   const plays = await spotify.totalPlays()
   revalidateTag('mal')
