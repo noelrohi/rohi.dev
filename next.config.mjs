84249fdb91e648484f20dd607df9484eeb4749e7
gneiru <noelrohi59@gmail.com>
2023-07-04 16:53:08 +0800

    remove server actions

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@ import "./env.mjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    serverActions: true
-  },
   images: {
     remotePatterns: [
       {
