94e11b182b0cce2cae6b2a910ca27138e4ee3b18
gneiru <noelrohi59@gmail.com>
2023-07-26 09:29:45 +0800

    add: toaster, nav, next-themes

---
+++ src/components/main-nav.tsx
@@ -0,0 +1,59 @@
+"use client"
+
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import * as React from "react"
+
+import { Icons } from "@/components/icons"
+import { MobileNav } from "@/components/mobile-nav"
+import { siteConfig } from "@/config/site"
+import { NavItem } from "@/lib/types"
+import { cn } from "@/lib/utils"
+
+interface MainNavProps {
+  items?: NavItem[]
+  children?: React.ReactNode
+}
+
+export function MainNav({ items, children }: MainNavProps) {
+  const path = usePathname()
+  const [showMobileMenu, setShowMobileMenu] = React.useState<boolean>(false)
+
+  return (
+    <div className="flex gap-6 md:gap-10">
+      <Link href="/" className="hidden items-center space-x-2 md:flex">
+        <Icons.logo />
+        <span className="hidden font-bold sm:inline-block">
+          {siteConfig.name}
+        </span>
+      </Link>
+      {items?.length ? (
+        <nav className="hidden gap-6 md:flex">
+          {items?.map((item, index) => (
+            <Link
+              key={index}
+              href={item.disabled ? "#" : item.href}
+              className={cn(
+                "flex items-center text-sm font-medium text-muted-foreground",
+                item.disabled && "cursor-not-allowed opacity-80",
+                path === item.href && "text-current underline opacity-100"
+              )}
+            >
+              {item.title}
+            </Link>
+          ))}
+        </nav>
+      ) : null}
+      <button
+        className="flex items-center space-x-2 md:hidden"
+        onClick={() => setShowMobileMenu(!showMobileMenu)}
+      >
+        {showMobileMenu ? <Icons.close /> : <Icons.logo />}
+        <span className="font-bold">Menu</span>
+      </button>
+      {showMobileMenu && items && (
+        <MobileNav items={items}>{children}</MobileNav>
+      )}
+    </div>
+  )
+}
