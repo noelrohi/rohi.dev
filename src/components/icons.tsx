3ff00137ffdf32f9a145e091b2c28de3ae0a3209
gneiru <noelrohi59@gmail.com>
2023-07-26 12:08:47 +0800

    change loader svg

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -87,20 +87,14 @@ export const Icons = {
     <svg
       width="1em"
       height="1em"
-      viewBox="0 0 48 48"
+      viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <g
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="4"
-      >
-        <path d="M4 24C4 35.0457 12.9543 44 24 44V44C35.0457 44 44 35.0457 44 24C44 12.9543 35.0457 4 24 4"></path>
-        <path d="M36 24C36 17.3726 30.6274 12 24 12C17.3726 12 12 17.3726 12 24C12 30.6274 17.3726 36 24 36V36"></path>
-      </g>
+      <path
+        fill="currentColor"
+        d="M3.055 13H5.07a7.002 7.002 0 0 0 13.858 0h2.016a9.001 9.001 0 0 1-17.89 0Zm0-2a9.001 9.001 0 0 1 17.89 0h-2.016A7.002 7.002 0 0 0 5.07 11H3.055Z"
+      ></path>
     </svg>
   ),
   close: (props: IconProps) => (
