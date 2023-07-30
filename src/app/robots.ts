23cedfd578624ee14c9b8d4225172c1606f526c4
gneiru <noelrohi59@gmail.com>
2023-07-30 14:43:07 +0800

    add host

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,8 +6,8 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: "/",
     },
     sitemap: `${siteConfig.url}/sitemap.xml`,
+    host: siteConfig.url,
   };
 }
