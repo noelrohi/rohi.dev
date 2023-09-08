e1dc6c5433df3368ec057f34b2b5439844b0df9b
gneiru <noelrohi59@gmail.com>
2023-09-08 13:52:48 +0800

    animated nav

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -1,15 +1,14 @@
 "use client";
 
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import * as React from "react";
-
 import { Icons } from "@/components/icons";
 import { MobileNav } from "@/components/mobile-nav";
 import { siteConfig } from "@/config/site";
-import { NavItem } from "@/types";
 import { cn } from "@/lib/utils";
-
+import { NavItem } from "@/types";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import * as React from "react";
+import { motion } from "framer-motion";
 interface MainNavProps {
   items?: NavItem[];
   children?: React.ReactNode;
@@ -18,6 +17,8 @@ interface MainNavProps {
 export function MainNav({ items, children }: MainNavProps) {
   const path = usePathname();
   const [showMobileMenu, setShowMobileMenu] = React.useState<boolean>(false);
+  const [isPending, startTransition] = React.useTransition();
+  const router = useRouter();
 
   return (
     <div className="flex gap-6 md:gap-10">
@@ -29,19 +30,30 @@ export function MainNav({ items, children }: MainNavProps) {
       </Link>
       {items?.length ? (
         <nav className="hidden gap-6 md:flex">
-          {items?.map((item, index) => (
-            <Link
-              key={index}
-              href={item.disabled ? "#" : item.href}
-              className={cn(
-                "flex items-center text-sm font-medium text-muted-foreground hover:border-b hover:border-foreground",
-                item.disabled && "cursor-not-allowed opacity-80",
-                path === item.href &&
-                  "border-b text-foreground border-foreground"
-              )}
-            >
-              {item.title}
-            </Link>
+          {items?.map((item) => (
+            <div className="relative" key={item.href}>
+              <button
+                onClick={() => {
+                  startTransition(() => {
+                    router.push(item.href);
+                  });
+                }}
+                className={cn(
+                  "relative items-center text-sm font-medium text-muted-foreground hover:border-foreground",
+                  item.disabled && "cursor-not-allowed opacity-80",
+                  path === item.href && "text-foreground border-foreground"
+                )}
+              >
+                {path === item.href && (
+                  <motion.span
+                    layoutId="bubble"
+                    className="rounded-full absolute z-10 bottom-0 left-0 flex w-full h-[1px] bg-blue-500"
+                    transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
+                  />
+                )}
+                {item.title}
+              </button>
+            </div>
           ))}
         </nav>
       ) : null}
