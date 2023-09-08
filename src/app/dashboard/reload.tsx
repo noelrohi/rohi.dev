f0248d77be23f25b2134c56f355d6498ed0c5c79
gneiru <noelrohi59@gmail.com>
2023-09-08 13:51:00 +0800

    using router hook instead of revalidate

--- src/app/dashboard/reload.tsx
+++ src/app/dashboard/reload.tsx
@@ -2,20 +2,19 @@
 
 import { Icons } from "@/components/icons";
 import { Button } from "@/components/ui/button";
-import { useTransition } from "react";
-import { revalidate } from "../_actions";
 import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
 
 export default function ReloadButton() {
   const [isPending, startTransition] = useTransition();
+  const router = useRouter();
 
   return (
     <>
       <Button
         className={cn("w-6 h-6 rounded-full", isPending && "animate-spin")}
-        onClick={() =>
-          startTransition(async () => await revalidate("/dashboard"))
-        }
+        onClick={() => startTransition(() => router.refresh())}
         size="icon"
         variant="ghost"
         disabled={isPending}
