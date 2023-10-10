e74e1f09faccb5533aa64279159bd00a37785f4a
gneiru <noelrohi59@gmail.com>
2023-10-10 12:33:03 +0800

    Add view transitions, Font

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const site = {
+  title: "Noel Rohi",
+  description:
+    "a weeb, swe based in Philippines",
+  url: "https://rohi.dev",
+};
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,14 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import { site } from '../consts';
+
+const {title, description} = site
 
 export async function GET(context) {
 	const posts = await getCollection('blog');
 	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
+		title,
+		description,
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
