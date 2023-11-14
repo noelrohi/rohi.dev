6a942291389ffbe21853cdc79bd689c49173618a
gneiru <noelrohi59@gmail.com>
2023-11-14 10:36:22 +0800

    cache content

---
+++ astro.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig, type AstroUserConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
+
+import tailwind from "@astrojs/tailwind";
+
+const config: AstroUserConfig = {
+  site: "https://www.rohi.dev",
+  integrations: [
+    mdx(),
+    sitemap(),
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+  experimental: {
+    contentCollectionCache: true,
+    devOverlay: true,
+    optimizeHoistedScript: true,
+  },
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+  markdown: {
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: "rose-pine-moon",
+      wrap: true,
+    },
+  },
+};
+
+// https://astro.build/config
+export default defineConfig(config);
