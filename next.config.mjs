b82ec5c963c73863c80ebefc9cf5d3f9c81ad9dc
gneiru <noelrohi59@gmail.com>
2024-01-27 23:40:31 +0800

    add redirects

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,39 @@ const nextConfig = {
     ppr: true,
     useLightningcss: true,
   },
-  redirects: async () => {
-    return [];
+  redirects: () => {
+    return [
+      {
+        destination: "https://github.com/gneiru/:slug*",
+        source: "/gh/:slug*",
+        permanent: true,
+      },
+      {
+        destination: "https://github.com/gneiru",
+        source: "/gh",
+        permanent: true,
+      },
+      {
+        destination: "https://github.com/gneiru/:slug*",
+        source: "/github/:slug*",
+        permanent: true,
+      },
+      {
+        destination: "https://github.com/gneiru",
+        source: "/github",
+        permanent: true,
+      },
+      {
+        destination: "https://www.linkedin.com/in/gneiru/",
+        source: "/linkedin",
+        permanent: true,
+      },
+      {
+        destination: "https://x.com/gneiru/",
+        source: "/twitter",
+        permanent: true,
+      },
+    ];
   },
   headers: () => {
     return [
