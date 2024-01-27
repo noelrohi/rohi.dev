1fe7435f5d1a7d164491e4137d7320c8b8c87753
gneiru <noelrohi59@gmail.com>
2024-01-27 17:02:12 +0800

    fix signin form

--- src/app/guestbook/@modal/(.)signin/page.tsx
+++ src/app/guestbook/@modal/(.)signin/page.tsx
@@ -1,28 +1,10 @@
-import { Button } from "@/components/ui/button";
+import SignIn from "../../signin/page";
 import { ModalDialog } from "../dialog";
-import { signIn } from "@/lib/auth";
 
 export default function Modal() {
   return (
     <ModalDialog>
-      <form className="w-full">
-        <Button
-          formAction={async () => {
-            "use server";
-            await signIn("google");
-          }}
-        >
-          Sign in with Google
-        </Button>
-        <Button
-          formAction={async () => {
-            "use server";
-            await signIn("discord", { redirectTo: "/guestbook" });
-          }}
-        >
-          Sign in with Discord
-        </Button>
-      </form>
+      <SignIn />
     </ModalDialog>
   );
 }
--- src/app/guestbook/signin/page.tsx
+++ src/app/guestbook/signin/page.tsx
@@ -1,25 +1,30 @@
 import { Button } from "@/components/ui/button";
 import { signIn } from "@/lib/auth";
 
-export default function Modal() {
+export default function SignIn() {
   return (
-    <form className="w-full">
-      <Button
-        formAction={async () => {
-          "use server";
-          await signIn("google");
-        }}
-      >
-        Sign in with Google
-      </Button>
-      <Button
-        formAction={async () => {
-          "use server";
-          await signIn("google");
-        }}
-      >
-        Sign in with Discord
-      </Button>
+    <form className="w-full space-y-4">
+      <div className="font-bold text-xl">Sign In Below</div>
+      <div className="flex gap-4">
+        <Button
+          className="w-full"
+          formAction={async () => {
+            "use server";
+            await signIn("google");
+          }}
+        >
+          Google
+        </Button>
+        <Button
+          className="w-full"
+          formAction={async () => {
+            "use server";
+            await signIn("google");
+          }}
+        >
+          Discord
+        </Button>
+      </div>
     </form>
   );
 }
