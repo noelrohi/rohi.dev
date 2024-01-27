70450d922f67a333d2391e06838a64d2c1a8f3aa
gneiru <noelrohi59@gmail.com>
2024-01-27 16:52:17 +0800

    add initial content for blog page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <section className="space-y-4">
+      <div className="font-mono text-muted-foreground text-sm">
+        read my blog
+      </div>
+    </section>
+  );
+}
