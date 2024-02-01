878549982c9704246ed7cea1d2afab2fe697b5f7
gneiru <noelrohi59@gmail.com>
2024-02-01 13:38:04 +0800

    Use resend sdk

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -1,20 +1,15 @@
 import { env } from "@/env.mjs";
+import { Resend } from "resend";
+
+const resend = new Resend(env.RESEND_API_KEY);
 
 export async function sendEmail(content: string) {
-  const data = await fetch("https://api.resend.com/emails", {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${env.RESEND_API_KEY}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      from: "guestbook@rohi.dev",
-      to: "n@rohi.dev",
-      subject: "New Guestbook Entry",
-      html: content,
-    }),
+  const { data, error } = await resend.emails.send({
+    from: "Guestbook <no-reply@rohi.dev>",
+    to: ["n@resend.dev"],
+    subject: "Hello world",
+    html: content,
   });
-
-  const response = await data.json();
-  console.log("Email sent", response);
+  if (error) console.error(error);
+  console.log("Email sent.", data);
 }
