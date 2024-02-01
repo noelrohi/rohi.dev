2330293b4f2b582d4473a871490d70d07ecd967d
gneiru <noelrohi59@gmail.com>
2024-02-01 13:57:46 +0800

    fix

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -5,10 +5,10 @@ const resend = new Resend(env.RESEND_API_KEY);
 
 export async function sendEmail(content: string) {
   const { data, error } = await resend.emails.send({
-    from: "Guestbook <no-reply@rohi.dev>",
-    to: ["n@resend.dev"],
-    subject: "Hello world",
-    html: content,
+    from: "Guestbook <guestbook@rohi.dev>",
+    to: ["n@rohi.dev"],
+    subject: "Guestbook Entry",
+    text: content,
   });
   if (error) console.error(error);
   console.log("Email sent.", data);
