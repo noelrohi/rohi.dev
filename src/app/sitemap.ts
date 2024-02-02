05de7c6761daa5dbae7f8a7f5e066794ed3278f8
gneiru <noelrohi59@gmail.com>
2024-02-02 14:02:11 +0800

    add blogpost to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,9 +1,18 @@
+import { getBlogPosts } from "@/lib/blog";
 import { links, projectURL } from "@/lib/consts";
+import { MetadataRoute } from "next";
 
-export default async function sitemap() {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const allBlogs = getBlogPosts();
   const routes = links.map(({ path }) => ({
     url: `${projectURL}${path}`,
     lastModified: new Date().toISOString().split("T")[0],
   }));
-  return routes;
+  const blogPostRoutes = allBlogs.map((blog) => ({
+    url: `${projectURL}/blog/${blog.slug}`,
+    lastModified: new Date(blog.metadata.publishedAt)
+      .toISOString()
+      .split("T")[0],
+  }));
+  return [...blogPostRoutes, ...routes];
 }
