da1858400ad549d1e1917bc294ef6ce2a82ca0a1
gneiru <noelrohi59@gmail.com>
2024-03-09 10:41:36 +0800

    ensure db url includes `postgres`

--- src/env.mjs
+++ src/env.mjs
@@ -3,7 +3,7 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    DATABASE_URL: z.string().url(),
+    DATABASE_URL: z.string().url().includes("postgres"),
     NEXTAUTH_SECRET: z.string().min(1),
     DISCORD_CLIENT_SECRET: z.string().min(1),
     DISCORD_CLIENT_ID: z.string().min(1),
