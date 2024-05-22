5bf72522300d10a4f4a005939d2242d05bb8dcaf
Rohi <n@rohi.dev>
2024-05-22 22:05:41 +0800

    update env.mjs (#84)
    update env.mjs
    
    opt out resend api key
    
    notFound when API_KEYs env vars are not set
    
    smaller height
    
    fmt

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
 export default function NotFound() {
   return (
-    <section className="flex min-h-[calc(100vh-120px)] items-center justify-center">
+    <section className="flex min-h-[calc(100vh-210px)] items-center justify-center">
       <h1 className="max-w-md scroll-m-20 text-center font-extrabold text-4xl tracking-tight lg:text-5xl">
         Can't seem to find this page. 🔍
       </h1>
