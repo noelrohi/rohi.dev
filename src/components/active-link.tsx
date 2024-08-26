ec57123e7f4f8b6a8f6d15e0ca12a93f31c5b0c9
gneiru <noelrohi59@gmail.com>
2024-08-26 17:01:19 +0800

    fix active-link

--- src/components/active-link.tsx
+++ src/components/active-link.tsx
@@ -6,27 +6,34 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useMemo } from "react";
 
+interface ActiveLinkProps extends React.ComponentPropsWithoutRef<typeof Link> {
+  parentProps?: React.ComponentPropsWithoutRef<"div">;
+}
+
 export function ActiveLink({
   children,
   className,
+  parentProps,
   ...props
-}: React.ComponentPropsWithoutRef<typeof Link>) {
+}: ActiveLinkProps) {
   const pathname = usePathname();
   const isActive = useMemo(() => {
     return pathname === props.href;
   }, [pathname, props.href]);
   return (
-    <Link
-      {...props}
+    <div
+      {...parentProps}
       className={cn(
         "relative rounded-md px-2 py-1 text-sm",
-        className,
+        parentProps?.className,
         !isActive ? "text-foreground" : "text-primary-foreground",
       )}
     >
-      <div className="relative z-10">{children}</div>
+      <Link className={cn("relative z-10", className)} {...props}>
+        {children}
+      </Link>
       {isActive && <ActiveIdentifier />}
-    </Link>
+    </div>
   );
 }
 
