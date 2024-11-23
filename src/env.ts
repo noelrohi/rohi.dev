c87e6513cf7dd05a6c7f7a1bd0585091f2c30aab
gneiru <noelrohi59@gmail.com>
2024-11-23 16:03:02 +0800

    run knip

--- src/env.ts
+++ src/env.ts
@@ -10,17 +10,7 @@ export const env = createEnv({
       .enum(["development", "test", "production"])
       .default("development"),
   },
-  server: {
-    DATABASE_URL: z.string().url().includes("postgres"),
-    NEXTAUTH_SECRET: z.string().min(1),
-    DISCORD_CLIENT_SECRET: z.string().min(1),
-    DISCORD_CLIENT_ID: z.string().min(1),
-    GOOGLE_CLIENT_SECRET: z.string().min(1),
-    GOOGLE_CLIENT_ID: z.string().min(1),
-    KNEXT_API_KEY: z.string().optional(),
-    LAST_FM_API_KEY: z.string().optional(),
-    RESEND_API_KEY: z.string().optional(),
-  },
+  server: {},
   client: {
     NEXT_PUBLIC_APP_URL: z.string().url(),
   },
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,17 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export function getRandomInt(param: { min: number; max: number }) {
-  const min = Math.ceil(param.min);
-  const max = Math.floor(param.max);
-  return Math.floor(Math.random() * (max - min) + min);
-}
-
-export function getNumberOfViews(date: string) {
-  const startedDate = new Date(date);
-  const dateNow = new Date();
-  const timeDiff = dateNow.getTime() - startedDate.getTime();
-  const numberOfViews = Math.floor(timeDiff / 1000 / 60 / 60 / 24);
-  return numberOfViews;
-}
