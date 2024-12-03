264b31e7bf7d8d96c0fd08ce0664bd1dece846c9
gneiru <noelrohi59@gmail.com>
2024-12-03 17:06:51 +0800

    rm all

--- src/app/not-found.tsx
+++
@@ -1,9 +0,0 @@
-export default function NotFound() {
-  return (
-    <section className="flex min-h-[calc(100vh-210px)] items-center justify-center">
-      <h1 className="max-w-md scroll-m-20 text-center font-extrabold text-4xl tracking-tight lg:text-5xl">
-        Can't seem to find this page. 🔍
-      </h1>
-    </section>
-  );
-}
