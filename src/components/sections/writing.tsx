737655020559882b5b641db7e760bd5b298d073e
gneiru <noelrohi59@gmail.com>
2024-12-04 08:39:11 +0800

    show all posts in index page

--- src/components/sections/writing.tsx
+++ src/components/sections/writing.tsx
@@ -3,11 +3,10 @@ import { allPosts } from "content-collections";
 import Link from "next/link";
 
 export function Writing() {
-  const posts = allPosts;
   return (
     <ShellSection index={5} title="Writing">
       <div className="grid grid-cols-1 gap-6 ">
-        {posts.map((post, index) => (
+        {allPosts.map((post, index) => (
           <Link key={index} href={post._meta.path}>
             <span className="text-muted-foreground">
               {new Date(post.time).toLocaleDateString()}
@@ -15,9 +14,6 @@ export function Writing() {
             {post.title}
           </Link>
         ))}
-        <Link href="/blog" className="font-medium text-[13px] hover:underline">
-          Read more posts
-        </Link>
       </div>
     </ShellSection>
   );
