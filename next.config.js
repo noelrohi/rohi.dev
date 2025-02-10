84a021eafb1d8d724f0b12ccb171a4b1eaa3bfbe
gneiru <noelrohi59@gmail.com>
2025-02-10 18:38:16 +0800

    chore(deps): upgrade Next.js to version 14.2.23 and related dependencies

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
         hostname: "utfs.io"
       }
     ]
-  }
+  },
 }
 
 // withContentCollections must be the outermost plugin
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,6 +53,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* <script src="https://unpkg.com/react-scan/dist/auto.global.js" async /> */}
+      </head>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
