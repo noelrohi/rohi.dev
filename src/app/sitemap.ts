97891187ab4cd025cf34b22c1b1720fee88fbb47
gneiru <noelrohi59@gmail.com>
2025-03-01 11:13:02 +0800

    fix: update sitemap to include blog route prefix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,7 +4,7 @@ import type { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const postRoutes = allPosts.map((post) => ({
-    url: `${projectURL}/${post._meta.path}`,
+    url: `${projectURL}/blog/${post._meta.path}`,
     lastModified: new Date(post.time).toISOString().split("T")[0],
   }));
   const routes = ["/"].map((url) => ({
